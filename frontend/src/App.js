3491052f2b83c1354db72e16670dc5d87afb04a5
BandaySajid <mbandaysajid@gmail.com>
2023-11-29 12:02:47 +0530

    added 404 page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,16 +6,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LogsState from './context/logs/LogsState.js';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import PageNotFound from './components/PageNotFound.js'
 
 function App() {
   return (
     <LogsState>
       <BrowserRouter>
-        <ToastContainer limit={2} autoClose={1000}/>
+        <ToastContainer limit={2} autoClose={1000} />
         <Routes>
           <Route path='/' element={<User />}>
           </Route>
           <Route path='/chat' element={<Chat />}>
+          </Route>
+          <Route path="*" element={<PageNotFound />}>
           </Route>
         </Routes>
       </BrowserRouter>
